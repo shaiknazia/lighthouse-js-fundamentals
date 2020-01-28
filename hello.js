fc98110d84177107b52fadd9ba13bb81993a263c
Nazia Shaik <shaiknazia.n@gmail.com>
2020-01-28 19:57:02 +0000

    hello.js

---
+++ hello.js
@@ -0,0 +1,8 @@
+// const sayHello  = function (name) {
+//   console.log("Hello, " + name);
+// }
+
+const sayHello  = function (name) {
+  return("Hello, " + name + "!");
+}
+console.log(sayHello("miranda")); 
