b94c9e82271408b4a9698ffbf456455fd163415e
Nazia Shaik <shaiknazia.n@gmail.com>
2020-02-02 21:39:47 +0000

    MapMethodLoop.js

---
+++ MapMethodLoop.js
@@ -0,0 +1,11 @@
+var bills = [50.23, 19.12, 34.01,
+  100.11, 12.15, 9.90, 29.11, 12.99,
+  10.00, 99.22, 102.20, 100.10, 6.77, 2.22
+];
+
+var totals = bills.map(function(bill) {
+  var tip = bill * 15/100;
+  totals = bill + tip;
+  return Number(totals.toFixed(2));
+});
+console.log(totals);
