7f400f89e86c6e3a2169443f7db1c09fe9ea151d
Nazia Shaik <shaiknazia.n@gmail.com>
2020-02-05 07:30:52 +0000

    Finding last index of a value in an array - LHL Exercise

---
+++ lastIndexOf.js
@@ -0,0 +1,13 @@
+function lastIndexOf(array, value) {
+  if (array.includes(value)) {
+    return (array.lastIndexOf(value));
+  } else {
+    return -1;
+  }
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
